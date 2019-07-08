2006f3a0a78cc03ce60d0f0b72c82bea30103c34
Dobriln1k <inikitoz13@gmail.com>
2019-07-08 12:01:35 +0300

    Final Version

--- com.example.testtasks/js/main.js
+++ com.example.testtasks/js/main.js
@@ -12,7 +12,50 @@
         themeManager.init();
                 
         $("#BUTTON").click(function () {
-            csInterface.evalScript('compareobj()');
+            const url = document.getElementById('URL');
+			const interval = document.getElementById('INTERVAL');
+			const log = document.getElementById('LOG');
+			const button = document.getElementById('BUTTON');
+
+var intervals;
+var button_click = 0;
+const set = new Set();
+
+		button.addEventListener('click', function() {
+			if(button_click % 2 === 0) {
+
+			const json = new XMLHttpRequest();
+			json.open("GET", url.value, false);
+			json.send();
+			var jsontext = JSON.parse(json.responseText);
+
+			button.innerHTML = "Stop";
+			intervals = setInterval(function() {
+			var i = 0;
+			var newjson = new XMLHttpRequest();
+			newjson.open('GET', url.value, false);
+			newjson.send();
+			var newjsontext = JSON.parse(newjson.responseText);
+
+			for(var keys in jsontext){
+				if(keys in newjsontext && jsontext[keys] === newjsontext[keys]) {
+					delete newjsontext[keys];
+				} 
+			}
+
+			for(var key in newjsontext) {
+				if(!set.has("Новое значение " + newjsontext[key] + " и его ключ " + key + "\n")) {
+					set.add("Новое значение " + newjsontext[key] + " и его ключ " + key + "\n");
+					log.value += "Новое значение " + newjsontext[key] + " и его ключ " + key + "\n";
+				}
+			}
+	}, interval.value) 
+		} else {
+			button.innerHTML = "Watch";
+			clearInterval(intervals)
+		}
+		button_click++;
+		});	
         });
     }
         
--- com.example.testtasks/jsx/hostscript.jsx
+++ com.example.testtasks/jsx/hostscript.jsx
@@ -2,49 +2,4 @@
 /*global $, Folder*/
 
 
-function compareobj() {	
-		const url = document.querySelector('#URL');
-		const interval = document.querySelector('#INTERVAL');
-		let log = document.querySelector('#LOG');
-		const button = document.querySelector('#BUTTON');
 
-let intervals;
-let button_click = 0;
-let set = new Set();
-
-		button.addEventListener('click', () => {
-			if(button_click % 2 === 0) {
-
-			const json = new XMLHttpRequest();
-			json.open("GET", url.value, false);
-			json.send();
-			let jsontext = JSON.parse(json.responseText);
-
-			button.innerHTML = "Stop";
-			intervals = setInterval(() => {
-			let i = 0;
-			let newjson = new XMLHttpRequest();
-			newjson.open('GET', url.value, false);
-			newjson.send();
-			let newjsontext = JSON.parse(newjson.responseText);
-
-			for(let key in jsontext){
-				if(key in newjsontext && jsontext[key] === newjsontext[key]) {
-					delete newjsontext[key];
-				} 
-			}
-
-			for(let key in newjsontext) {
-				if(!set.has(`Новое значение ${newjsontext[key]} и его ключ ${key}\n`)) {
-					set.add(`Новое значение ${newjsontext[key]} и его ключ ${key}\n`);
-					log.value += `Новое значение ${newjsontext[key]} и его ключ ${key}\n`;
-				}
-			}
-	}, interval.value) 
-		} else {
-			button.innerHTML = "Watch";
-			clearInterval(intervals)
-		}
-		button_click++;
-		});	
-}
